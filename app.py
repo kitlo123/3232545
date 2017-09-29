ca4646479d5d676263523e45c5a8bf73975288e7
kitlo123 <qut88@hotmail.com>
2017-09-29 13:04:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 line_bot_api = LineBotApi(config['kit']['qut881@gmail.com'])
-handler = WebhookHandler(config['kit']['12345678'])
+handler = WebhookHandler(config['kit']['qw1234'])
 
 @app.route("/callback", methods=['POST'])
 def callback():
